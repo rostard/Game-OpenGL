3514ee976b39fcccddb36cf6f28d3a8a5943ebb9
rostard <kos.boykow@gmail.com>
2017-10-16 13:26:19 +0300

    Add texture class

---
+++ texture.cpp
@@ -0,0 +1,5 @@
+//
+// Created by rostard on 16.10.17.
+//
+
+#include "texture.h"
---
+++ texture.h
@@ -0,0 +1,8 @@
+//
+// Created by rostard on 16.10.17.
+//
+
+#ifndef GAME_OPENGL_TEXTURE_H
+#define GAME_OPENGL_TEXTURE_H
+
+#endif //GAME_OPENGL_TEXTURE_H
