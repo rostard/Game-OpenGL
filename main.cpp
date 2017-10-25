464c8f314b634d48da49e88afc16ace5f1b4622e
rostard <kos.boykow@gmail.com>
2017-10-25 19:40:32 +0300

    Add net test

--- main.cpp
+++ main.cpp
@@ -8,13 +8,19 @@
 #include <GLFW/glfw3.h>
 #include <ft2build.h>
 #include <string>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <thread>
+#include <zconf.h>
 
 #include "font.h"
 #include "game.h"
-#include "resource_mager.h"
+
 
 
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void mouse_callback(GLFWwindow* window, int button, int action, int modes);
 
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
@@ -47,8 +53,68 @@ int Init(){
 
 
     glfwSetKeyCallback(window, key_callback);
-
+    glfwSetMouseButtonCallback(window, mouse_callback);
     return 0;
+}
+
+
+void CreateServer(){
+    std::cerr<<"create"<<std::endl;
+    sockaddr_in addr;
+    addr.sin_port = htons(12345);
+    addr.sin_family = AF_INET;
+    inet_aton("0.0.0.0",&addr.sin_addr);
+
+
+    int fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    int err =bind(fd, (sockaddr*)&addr, sizeof(addr));
+    if(err == -1){
+        std::cerr<<"Bind error: "<<strerror(errno)<<std::endl;
+        exit(EXIT_FAILURE);
+    }
+    err = listen(fd, SOMAXCONN);
+    if(err == -1){
+        std::cerr<<"Listen error: "<<strerror(errno)<<std::endl;
+        exit(EXIT_FAILURE);
+    }
+    int client = accept(fd, nullptr, nullptr);
+    char buf[1024];
+    int l;
+    while(l = recv(client, buf, 1024, 0)){
+        std::stringstream ss(buf);
+        int x, y, n;
+        ss>>x>>y>>n;
+        for(int i = 0; i<1024;i ++)buf[i]=' ';
+        if(x+y!=n)continue;
+        GameName.secondPos.x = x;
+        GameName.secondPos.y = y;
+
+    }
+}
+
+void ConnectToServer(){
+    std::cerr<<"connect"<<std::endl;
+    sockaddr_in addr;
+    addr.sin_port = htons(12345);
+    addr.sin_family = AF_INET;
+    int fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    //int err =bind(fd, (sockaddr*)&addr, sizeof(addr));
+
+    inet_aton("127.0.0.1", &addr.sin_addr);
+
+    int res = connect(fd, (sockaddr*)&addr, sizeof(addr));
+    if(res == -1){
+        std::cerr<<"error connect: "<<strerror(errno)<<std::endl;
+    }
+    while(true){
+        std::stringstream ss;
+        ss<<GameName.ownPos.x<<" "<<GameName.ownPos.y<<" ";
+        int n = GameName.ownPos.x + GameName.ownPos.y;
+        ss<<n;
+
+        send(fd, ss.str().c_str(), ss.str().length(), 0);
+
+    }
 }
 
 int main(){
@@ -59,7 +125,7 @@ int main(){
     glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    glEnable(GL_CULL_FACE);
+    //glEnable(GL_CULL_FACE);
 
     GameName.Init();
 
@@ -69,6 +135,14 @@ int main(){
     GLfloat dsum =0.0f;
     int frames = 0;
     int FPS;
+
+
+    //std::thread net(CreateServer);
+
+    std::thread net(ConnectToServer);
+
+    Shader simple = ResourceManager::LoadShader("shaders/simple.vs.glsl", "shaders/simple.fs.glsl", nullptr, "simple");
+
     while(!glfwWindowShouldClose(window)){
         //Time calculation
         frames ++;
@@ -87,7 +161,7 @@ int main(){
         GameName.Update(deltaTime);
 
         // Rendering
-        glClearColor(0.0, 0.0, 0.0, 1.0);
+        glClearColor(0.3, 0.0, 0.1, 1.0);
         glClear(GL_COLOR_BUFFER_BIT);
 
         GameName.Render();
@@ -96,7 +170,7 @@ int main(){
         glfwPollEvents();
 
     }
-
+    //net.join();
     ResourceManager::Clear();
     glfwTerminate();
     return 0;
@@ -107,10 +181,16 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
         glfwSetWindowShouldClose(window, true);
     }
     if(key >= 0 && key <1024){
+        //std::cerr<<key<<std::endl;
         if(action == GLFW_PRESS)
             GameName.Keys[key] = GL_TRUE;
         else if(action == GLFW_RELEASE)
             GameName.Keys[key] = GL_FALSE;
 
     }
+}
+
+void mouse_callback(GLFWwindow* window, int button, int action, int modes){
+    double xpos, ypos;
+    glfwGetCursorPos(window, &xpos, &ypos);
 }
